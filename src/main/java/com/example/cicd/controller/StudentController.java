fc6bed95f5eaea8f54f492a43613186577387c66
samruddhidpatil <samruddhidpatil2004@gmail.com>
2026-02-18 08:18:48 +0530

    Init

---
+++ src/main/java/com/example/cicd/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.example.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/student")
+    public String getStudent(){
+        return "student";
+    }
+}
